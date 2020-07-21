e4fb6ac3d795186dcc72cb46846cbb9feb2d23b1
Berlin Galvan <galvanberlin@gmail.com>
2020-07-20 21:37:45 -0700

    Revert "added new pages,links, and content"

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -6,10 +6,7 @@
       </strong>
       <nav class="nav">
         <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/our-story/">Our Story</g-link>
-        <g-link class="nav__link" to="/travel-and-stay/">Travel & Stay</g-link>
-        <a class="nav__link" href="https://www.zola.com/registry/berlinandmichael" target="_blank" rel="noopener">Registry</a>
-        <g-link class="nav__link" to="/rsvp/">RSVP</g-link>
+        <g-link class="nav__link" to="/about/">About</g-link>
       </nav>
     </header>
     <slot/>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,37 +4,11 @@
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
     <g-image alt="Example image" src="~/favicon.png" width="135" />
 
-    <h2>We're getting married!</h2>
-    <h1>Michael & Berlin</h1>
-    <h2>06.26.21</h2>
+    <h1>Hello, world!</h1>
 
-
-    <h1>Ceremony</h1>
-    <h2>4:00 PM</h2>
     <p>
-      Front Lawn <br>
-      Olivas Adobe Historic Park <br>
-      4200 Olivas Park Drive <br>
-      San Buenaventura, California 93001
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
     </p>
-    <p>
-      <a href="https://goo.gl/maps/Pwi6ZTkzNBYTkga38" target="_blank" rel="noopener">Map</a>
-    </p>
-
-    <h1>Reception</h1>
-    <h2>4:30 PM - 10:30 PM</h2>
-    <p>
-      Plaza <br>
-      Olivas Adobe Historic Park <br>
-      4200 Olivas Park Drive <br>
-      San Buenaventura, California 93001
-    </p>
-    <p>
-      <a href="https://goo.gl/maps/Pwi6ZTkzNBYTkga38" target="_blank" rel="noopener">Map</a>
-    </p>
-
-    <h1>See how it all started</h1>
-    <button>Our Story</button>
 
     <p class="home-links">
       <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
@@ -47,7 +21,7 @@
 <script>
 export default {
   metaInfo: {
-    title: 'Berlin & Michael Wedding'
+    title: 'Hello, world!'
   }
 }
 </script>
