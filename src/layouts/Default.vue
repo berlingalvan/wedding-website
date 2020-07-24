6e5c051c0603cd8f725b7736225a8ab642da9788
Berlin Galvan <galvanberlin@gmail.com>
2020-07-23 22:19:10 -0700

    Edited home page styles

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,15 +1,29 @@
 <template>
   <div class="layout">
     <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/our-story/">Our Story</g-link>
-        <g-link class="nav__link" to="/travel-and-stay/">Travel & Stay</g-link>
-        <a class="nav__link" href="https://www.zola.com/registry/berlinandmichael" target="_blank" rel="noopener">Registry</a>
-        <g-link class="nav__link" to="/rsvp/">RSVP</g-link>
+      <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
+        <div class="container is-fluid">
+          <div class="navbar-menu">
+            <div class="navbar-start p-2">
+              <g-link class="navbar-item" to="/">Home</g-link>
+              <g-link class="navbar-item" to="/our-story/">Our Story</g-link>
+              <g-link class="navbar-item" to="/travel-and-stay/">Travel & Stay</g-link>
+              <a class="navbar-item" href="https://www.zola.com/registry/berlinandmichael" target="_blank" rel="noopener">Registry</a>
+            </div>
+            <div class="navbar-end">
+              <div class="navbar-item">
+                <div class="buttons">
+                  <g-link class="button is-primary" to="/rsvp/">RSVP</g-link>
+                </div>
+              </div>
+            </div>
+            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+          </div>
+        </div>
       </nav>
     </header>
     <slot/>
@@ -23,3 +37,4 @@ query {
   }
 }
 </static-query>
+
